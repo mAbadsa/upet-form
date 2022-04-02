7d18dc3c95f83e79dc8ec32d715e95d856bc2595
mAbadsa <moh22389@gmail.com>
2022-04-02 21:12:45 +0300

    ...

--- server/db/connect.ts
+++ server/db/connect.ts
@@ -1,3 +1,4 @@
+require("dotenv").config();
 import { connect, createConnection, Connection } from "mongoose";
 
 const connectDB = async (): Promise<void> => {
@@ -10,6 +11,7 @@ const connectDB = async (): Promise<void> => {
     if (process.env.NODE_ENV === "development") {
       await connect("mongodb://localhost:27017/upet");
     }
+    // await connect("mongodb://localhost:27017/upet");
     console.log("MongoDB connected!");
   } catch (error: any) {
     console.error("db:config::", error.message);
